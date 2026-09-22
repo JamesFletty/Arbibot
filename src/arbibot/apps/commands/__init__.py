b3e2add8fb315318cce8bab5225bbb784f1a9424
James Fletty <81728344+JamesFletty@users.noreply.github.com>
2026-09-22 03:14:04 -0400

    Export synchronized session recorder

--- src/arbibot/apps/commands/__init__.py
+++ src/arbibot/apps/commands/__init__.py
@@ -2,6 +2,7 @@ from arbibot.apps.commands.benchmark_repricing import run_benchmark_repricing
 from arbibot.apps.commands.paper import run_paper
 from arbibot.apps.commands.record_binance import run_record_binance
 from arbibot.apps.commands.record_polymarket import run_record_polymarket
+from arbibot.apps.commands.record_session import run_record_session
 from arbibot.apps.commands.replay import run_replay
 from arbibot.apps.commands.status import run_status
 from arbibot.apps.commands.validate_config import run_validate_config
@@ -11,6 +12,7 @@ __all__ = [
     "run_paper",
     "run_record_binance",
     "run_record_polymarket",
+    "run_record_session",
     "run_replay",
     "run_status",
     "run_validate_config",
