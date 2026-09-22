42d482428a156fae87f4ad57effc5c81029f0c01
James Fletty <81728344+JamesFletty@users.noreply.github.com>
2026-09-22 03:09:43 -0400

    Export Polymarket recorder command

--- src/arbibot/apps/commands/__init__.py
+++ src/arbibot/apps/commands/__init__.py
@@ -1,6 +1,7 @@
 from arbibot.apps.commands.benchmark_repricing import run_benchmark_repricing
 from arbibot.apps.commands.paper import run_paper
 from arbibot.apps.commands.record_binance import run_record_binance
+from arbibot.apps.commands.record_polymarket import run_record_polymarket
 from arbibot.apps.commands.replay import run_replay
 from arbibot.apps.commands.status import run_status
 from arbibot.apps.commands.validate_config import run_validate_config
@@ -9,6 +10,7 @@ __all__ = [
     "run_benchmark_repricing",
     "run_paper",
     "run_record_binance",
+    "run_record_polymarket",
     "run_replay",
     "run_status",
     "run_validate_config",
